24183899ce979ca650306291d0ef7afe96d346c4
Ahmed Hady Hassan <ahmed.hady@flairstech.com>
2026-01-14 06:16:30 +0200

    .

--- web-angular/src/environments/environment.prod.ts
+++ web-angular/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://api.bazar-zone.com',
+    apiUrl: 'https://api.bazar-zone.com',
     oAuthConfig: {
-        issuer: 'http://api.bazar-zone.com',
+        issuer: 'https://api.bazar-zone.com',
         clientId: 'BazarZone_App',
         scope: 'openid email phone profile roles BazarZone offline_access',
     },
